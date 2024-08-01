ee50d5919f7f4f8d5952c7944759adacdce32228
sana <sanaekram123@gmail.com>
2024-08-01 22:13:48 +0530

    ye

--- app/page.js
+++ app/page.js
@@ -1,7 +1,7 @@
 'use client'
 import Image from "next/image";
 import {useState, useEffect} from 'react'
-import { firestore } from '@firebase'
+import { firestore } from '@/firebase'
 import {Box, Typography} from '@mui/material'
 
 export default function Home() {
