7148be41cdbbedb33b29427a124f109754d6b6f1
Reebaree <123104240+Reebaree@users.noreply.github.com>
2024-08-06 18:19:26 +0530

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -11,7 +11,7 @@ export default function RootLayout({ children }) {
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <title>{metadata.title}</title>
         <meta name="description" content={metadata.description} />
-        <link rel="shortcut icon" href="https://www.svgrepo.com/show/449636/boxes.svg" type="image/x-icon" />
+        
       </head>
       <body>
         <main>{children}</main>
